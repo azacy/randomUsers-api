427acbd671fcd70087192e8f5bb3e49111a18d11
azacy <enejustin725@gmail.com>
2022-09-04 21:59:19 +0100

    updatedApi

--- index.js
+++ index.js
@@ -4,5 +4,6 @@
     let roundUp = newUser.results
     let profile = roundUp.map(user=> `<center><img src="${user.picture.large}"><br><br> <strong>First Name: ${user.name.first}<br>Last Name:${user.name.last}</strong></center>`)
     document.body.innerHTML = profile
+setInterval(randomusers, 2000)
 } 
  randomusers()
